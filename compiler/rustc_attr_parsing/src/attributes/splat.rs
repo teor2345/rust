c0064b6c87d4268b864881b36bb56198e7139c0e
teor <teor@riseup.net>
2026-03-19 15:42:44 +1000

    fixup! fixup! squash! Appease tidy

--- compiler/rustc_attr_parsing/src/attributes/splat.rs
+++ compiler/rustc_attr_parsing/src/attributes/splat.rs
@@ -8,7 +8,10 @@ pub(crate) struct SplatParser;
 impl<S: Stage> NoArgsAttributeParser<S> for SplatParser {
     const PATH: &[Symbol] = &[sym::splat];
     const ON_DUPLICATE: OnDuplicate<S> = OnDuplicate::Warn;
-    const ALLOWED_TARGETS: AllowedTargets =
-        AllowedTargets::AllowList(&[Allow(Target::Param), Warn(Target::MacroCall)]);
+    const ALLOWED_TARGETS: AllowedTargets = AllowedTargets::AllowList(&[
+        Allow(Target::Param),
+        // FIXME(splat): only allow MacroCall if the macro creates an argument
+        Allow(Target::MacroCall),
+    ]);
     const CREATE: fn(Span) -> AttributeKind = AttributeKind::Splat;
 }
